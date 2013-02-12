46cb0dc2f353ea34f3daa84c17c6493d89fdd13d
kanfet <byte.developer@gmail.com>
2013-02-13 03:04:28 +0600

    migration with limit for mysql2 adapter

--- db/migrate/001_add_encrypted_text_to_wiki_contents.rb
+++ db/migrate/001_add_encrypted_text_to_wiki_contents.rb
@@ -1,5 +1,9 @@
 class AddEncryptedTextToWikiContents < ActiveRecord::Migration
   def change
-    add_column WikiContent.table_name, :encrypted_text, :text
+    if ActiveRecord::Base.connection.adapter_name  == 'Mysql2'
+      add_column WikiContent.table_name, :encrypted_text, :text, limit: 4294967295
+    else
+      add_column WikiContent.table_name, :encrypted_text, :text
+    end
   end
 end
