a6641c150eb816acdb96177bc71d5c9673e371bd
Igor Olemskoi <igor@southbridge.ru>
2015-12-22 22:54:37 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_wiki_encryptor do
   name 'Redmine Wiki Encryptor plugin'
   author 'Pavel Nemkin'
   description 'Plugin encrypts wiki content in database'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/olemskoi/redmine_wiki_encryptor'
   author_url 'http://centos-admin.ru'
 end
