a8b5acc3130b46916a6ba45d65efd62c3635194b
cross <cross@vgg.ru>
2016-11-25 22:34:49 +0300

    Rewrite tests for search.

--- test/unit/wiki_page_patch_test.rb
+++ test/unit/wiki_page_patch_test.rb
@@ -28,85 +28,91 @@ class WikiPagePatchTest < ActiveSupport::TestCase
 
   def test_search_by_anonymous
     User.current = nil
-    result = WikiPage.search(@tokens).first
-    assert result.include?(@page_with_matching_title)
-    assert result.include?(@page_with_matching_text)
+    result = WikiPage.search_result_ranks_and_ids(@tokens)
+    assert_includes result, wiki_rand_and_id(@page_with_matching_title)
+    assert_includes result, wiki_rand_and_id(@page_with_matching_text)
   end
 
   def test_search_by_anonymous_without_permission
     User.current = nil
     remove_permission Role.anonymous, :view_wiki_pages
-    result = WikiPage.search(@tokens).first
-    assert !result.include?(@page_with_matching_title)
-    assert !result.include?(@page_with_matching_text)
+    result = WikiPage.search_result_ranks_and_ids(@tokens)
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_title))
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_text))
   end
 
   def test_search_by_anonymous_in_private_project
     User.current = nil
     @project.update_attribute :is_public, false
-    result = WikiPage.search(@tokens).first
-    assert !result.include?(@page_with_matching_title)
-    assert !result.include?(@page_with_matching_text)
+    result = WikiPage.search_result_ranks_and_ids(@tokens)
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_title))
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_text))
   end
 
   def test_search_by_user
     User.current = User.find_by_login('rhill')
     assert User.current.memberships.empty?
-    result = WikiPage.search(@tokens).first
-    assert result.include?(@page_with_matching_title)
-    assert result.include?(@page_with_matching_text)
+    result = WikiPage.search_result_ranks_and_ids(@tokens)
+    assert_includes result, wiki_rand_and_id(@page_with_matching_title)
+    assert_includes result, wiki_rand_and_id(@page_with_matching_text)
   end
 
   def test_search_by_user_without_permission
     User.current = User.find_by_login('rhill')
     assert User.current.memberships.empty?
     remove_permission Role.non_member, :view_wiki_pages
-    result = WikiPage.search(@tokens).first
-    assert !result.include?(@page_with_matching_title)
-    assert !result.include?(@page_with_matching_text)
+    result = WikiPage.search_result_ranks_and_ids(@tokens)
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_title))
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_text))
   end
 
   def test_search_by_user_in_private_project
     User.current = User.find_by_login('rhill')
     assert User.current.memberships.empty?
     @project.update_attribute :is_public, false
-    result = WikiPage.search(@tokens).first
-    assert !result.include?(@page_with_matching_title)
-    assert !result.include?(@page_with_matching_text)
+    result = WikiPage.search_result_ranks_and_ids(@tokens)
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_title))
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_text))
   end
 
   def test_search_by_allowed_member
     User.current = User.find_by_login('jsmith')
     assert User.current.projects.include?(@project)
-    result = WikiPage.search(@tokens).first
-    assert result.include?(@page_with_matching_title)
-    assert result.include?(@page_with_matching_text)
+    result = WikiPage.search_result_ranks_and_ids(@tokens)
+    assert_includes result, wiki_rand_and_id(@page_with_matching_title)
+    assert_includes result, wiki_rand_and_id(@page_with_matching_text)
   end
 
   def test_search_by_allowed_member_in_private_project
     User.current = User.find_by_login('jsmith')
     assert User.current.projects.include?(@project)
     @project.update_attribute :is_public, false
-    result = WikiPage.search(@tokens).first
-    assert result.include?(@page_with_matching_title)
-    assert result.include?(@page_with_matching_text)
+    result = WikiPage.search_result_ranks_and_ids(@tokens)
+    assert_includes result, wiki_rand_and_id(@page_with_matching_title)
+    assert_includes result, wiki_rand_and_id(@page_with_matching_text)
   end
 
   def test_search_by_title_only
-    result = WikiPage.search(@tokens, nil, titles_only: true).first
-    assert result.include?(@page_with_matching_title)
-    assert !result.include?(@page_with_matching_text)
+    User.current = User.find_by_login('jsmith')
+    result = WikiPage.search_result_ranks_and_ids(@tokens, User.current, nil, titles_only: true)
+    assert_includes result, wiki_rand_and_id(@page_with_matching_title)
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_text))
   end
 
   def test_search_by_all_words
-    result = WikiPage.search(@tokens, nil, all_words: true).first
-    assert !result.include?(@page_with_matching_title)
-    assert !result.include?(@page_with_matching_text)
-    assert result.include?(@page_with_matching_title_all_words)
-    assert result.include?(@page_with_matching_text_all_words)
+    User.current = User.find_by_login('jsmith')
+    result = WikiPage.search_result_ranks_and_ids(@tokens, User.current, nil, all_words: true)
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_title))
+    assert !result.include?(wiki_rand_and_id(@page_with_matching_text))
+    assert_includes result, wiki_rand_and_id(@page_with_matching_title_all_words)
+    assert_includes result, wiki_rand_and_id(@page_with_matching_text_all_words)
   end
 
   private
+
+  def wiki_rand_and_id(wiki)
+    [wiki.created_on.to_i, wiki.id]
+  end
 
   def remove_permission(role, permission)
     role.permissions = role.permissions - [ permission ]
